3556c13f0c4b0923cb23cc22894e0ea944dd9e6b
Mario Oliver <mar.mariooliver93@gmail.com>
2023-07-18 12:34:14 -0400

    added react-toastify to the project

--- 04-fundamental-projects/09-color-generator/starter/src/App.jsx
+++ 04-fundamental-projects/09-color-generator/starter/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react';
 import ColorList from './components/ColorList';
 import Form from './components/Form';
 import Values from 'values.js';
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
   const colorArray = new Values('#f15025').all(10);
   const [colors, setColors] = useState(colorArray);
+  // toast.error('error');
+  // toast.success('success');
   return (
     <main>
       <Form></Form>
       <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   );
 };
