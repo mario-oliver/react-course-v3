471120ea3bf26b6a69f04803de4ef5d7b77453e1
Mario Oliver <mar.mariooliver93@gmail.com>
2023-07-18 12:14:46 -0400

    imported values.js and utilizing it to statically create a list of 21 color hues/tints

--- 04-fundamental-projects/09-color-generator/starter/src/App.jsx
+++ 04-fundamental-projects/09-color-generator/starter/src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import ColorList from './components/ColorList';
 import Form from './components/Form';
+import Values from 'values.js';
 
 const App = () => {
+  const colorArray = new Values('#f15025').all(10);
+  const [colors, setColors] = useState(colorArray);
   return (
     <main>
       <Form></Form>
