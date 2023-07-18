1aa77a0f049e62fe737f31a0cb913a91171423fe
Mario Oliver <mar.mariooliver93@gmail.com>
2023-07-18 12:06:22 -0400

    updated form to include form, placeholder functions, and all neceserarry boilerplate for how the user will submit a color to get the list of colors

--- 04-fundamental-projects/09-color-generator/starter/src/components/Form.jsx
+++ 04-fundamental-projects/09-color-generator/starter/src/components/Form.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
-  return <div>Form</div>;
+  const [color, setColor] = useState('');
+  const handleSubmit = (e) => {};
+  return (
+    <section className="container">
+      <h4>color generator</h4>
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          placeholder="#f15025"
+        />
+        <button
+          className="btn"
+          type="submit"
+          style={{ backgroundColor: color }}
+        >
+          Submit
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default Form;
