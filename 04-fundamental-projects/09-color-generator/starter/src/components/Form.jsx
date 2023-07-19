d4067e610a48ac0873fd69472e9f2a604467fd4f
Mario Oliver <mar.mariooliver93@gmail.com>
2023-07-19 16:58:30 -0400

    setting up complementary colors and new input logic

--- 04-fundamental-projects/09-color-generator/starter/src/components/Form.jsx
+++ 04-fundamental-projects/09-color-generator/starter/src/components/Form.jsx
@@ -2,7 +2,34 @@ import React, { useState } from 'react';
 
 const Form = () => {
   const [color, setColor] = useState('');
+  const [compColor, setCompColor] = useState('');
   const handleSubmit = (e) => {};
+
+  const generateHex = () => {
+    let hexColor = '#';
+    const hexValues = '0123456789abcdef';
+    for (let i = 0; i < 6; i++) {
+      hexColor += hexValues[Math.floor(Math.random() * 16)];
+    }
+    return hexColor;
+  };
+
+  // create a function that generates a complementary color hex valued based on a hex value passed in
+  const generateComplementaryHex = (hexValue) => {
+    // convert the hex value to rgb
+    const rgbValue = hexToRgb(hexValue);
+    // convert the rgb value to hsl
+    const hslValue = rgbToHsl(rgbValue);
+    // add 180 to the hsl value's hue
+    hslValue.hue += 180;
+    // convert the hsl value back to rgb
+    const newRgbValue = hslToRgb(hslValue);
+    // convert the rgb value back to hex
+    const newHexValue = rgbToHex(newRgbValue);
+    // return the new hex value
+    return newHexValue;
+  };
+
   return (
     <section className="container">
       <h4>color generator</h4>
@@ -10,7 +37,9 @@ const Form = () => {
         <input
           type="color"
           value={color}
-          onChange={(e) => setColor(e.target.value)}
+          onChange={(e) => {
+            setColor(e.target.value);
+          }}
         />
         <input
           type="text"
@@ -23,9 +52,31 @@ const Form = () => {
           type="submit"
           style={{ backgroundColor: color }}
         >
-          Submit
+          Set Color
         </button>
       </form>
+      <h4>complementary color</h4>
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
+          type="text"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+      </form>
+      <br></br>
+      <button
+        className="btn"
+        onClick={() => {
+          setColor(generateHex);
+        }}
+      >
+        Random Color
+      </button>
     </section>
   );
 };
